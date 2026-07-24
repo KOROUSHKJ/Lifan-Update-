2839dbafa8711953d56a5112fba6db3dc0d6b646
KOROUSHKJ <76644310+KOROUSHKJ@users.noreply.github.com>
2026-07-24 11:44:53 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1916,7 +1916,9 @@ if __name__ == "__main__":
     uvicorn.run(
         app, 
         host="0.0.0.0", 
-        port=CONFIG["port"], 
-        ssl_certfile="cert.pem", 
+        port=CONFIG["port"]
+    )
+
+   ssl_certfile="cert.pem", 
         ssl_keyfile="key.pem"
     )
